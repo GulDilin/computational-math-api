e82e60e010db3b1086b98113282e1f3a0159aa8d
GulDilin <zhenyagurin@gmail.com>
2020-04-27 00:21:50 +0300

    refactor app creation

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,11 @@ from .api import LagrangeApprox
 from flask_restful import Api
 from flask_cors import CORS
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
 
-    CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-    api = Api(app, prefix='/api')
-    api.add_resource(LagrangeApprox, '/')
-    return app
+api = Api(app, prefix='/api')
+api.add_resource(LagrangeApprox, '/')
+

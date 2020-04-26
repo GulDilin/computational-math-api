a30e9f5dd3c80fe7c4a56cac830ded80fe710117
GulDilin <zhenyagurin@gmail.com>
2020-04-26 12:20:25 +0300

    add lagrange function approximation method

---
+++ counter/lagrang.py
@@ -0,0 +1,10 @@
+from functools import reduce
+
+
+def l(f, x, new_x, ind):
+    return reduce(lambda k, y: k * y,
+                  [(new_x - x[i]) / (x[ind] - x[i]) for i in range(len(x)) if i != ind]) * f(x[ind])
+
+
+def approx(f, x, new_x):
+    return sum([l(f, x, new_x, i) for i in range(len(x))])

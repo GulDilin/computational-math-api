bb1939e435f47da2c76e3b69ea95d5605aacfe9b
Guldilin <zhenyagurin@gmail.com>
2020-06-04 13:17:39 +0300

    register new class to api

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .api import LagrangeApprox
+from .api import DifferentialEquations
 from flask_restful import Api
 from flask_cors import CORS
 
@@ -10,4 +11,5 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 api = Api(app, prefix='/api')
 api.add_resource(LagrangeApprox, '/')
+api.add_resource(DifferentialEquations, '/diff_eq')
 
